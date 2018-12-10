6840a2f76356c7845f882311e8e392c055f33766
micuat <micuat@gmail.com>
2018-12-10 20:05:59 +0900

    triangles

--- src/js/P5Sketches/nh1.js
+++ src/js/P5Sketches/nh1.js
@@ -1,5 +1,3 @@
-import p5 from 'p5'
-
 module.exports = (p) => {
     p.preload = () => {
 
@@ -11,41 +9,61 @@ module.exports = (p) => {
         p.background(240);
         p.frameRate(30);
 
-        let n = 30;
+        let n = 10;
         for(let i = 0; i < n; i++) {
-            let x = p.random(p.width);
-            let y = p.random(p.height);
-            x = Math.floor(x * 0.01) / 0.01;
-            y = Math.floor(y * 0.01) / 0.01;
-            points.push({x: x, y: y, target: Math.floor(p.random(n))});
+            let qn = Math.pow(2, Math.floor(p.map(i, 0, n, 3, 8)));
+            let y = p.map(qn, 0, Math.pow(2, 7), 0, p.height);
+            let x = Math.floor(p.random(p.width) / qn) * qn;
+            points.push({x: x, y: y, target0: Math.floor(p.random(n)), target1: Math.floor(p.random(n))});
         }
     }
 
     p.draw = () => {
         p.background(240);
-        p.fill(0);
+        p.noFill();
 
         if(p.frameCount % 120 == 0) {
             for(let pt of points) {
-                pt.target = Math.floor(p.random(points.length));
+                pt.target0 = Math.floor(p.random(points.length));
+                pt.target1 = Math.floor(p.random(points.length));
             }
         }
+
+        let alpha = 1;
 
         let t = (p.frameCount / 30.0) * 0.25;
         t = t - Math.floor(t);
         let t0 = t > 0.5 ? (t - 0.5) * 2 : 0;
         let t1 = t < 0.5 ? t * 2 : 1;
         for(let pt of points) {
-            let ptt = points[pt.target];
-            // p.ellipse(pt.x, pt.y, 10);
-            let x0 = p.lerp(pt.x, ptt.x, t0);
-            let y0 = p.lerp(pt.y, ptt.y, t0);
-            let x1 = p.lerp(pt.x, ptt.x, t1);
-            let y1 = p.lerp(pt.y, ptt.y, t1);
-            p.stroke(81,97,242, (0.5 - Math.abs(t - 0.5)) * 32);
-            p.line(pt.x, pt.y, ptt.x, ptt.y);
-            p.stroke(81,97,242, 32);
-            p.line(x0, y0, x1, y1);
+            let ptt0 = points[pt.target0];
+            let ptt1 = points[pt.target1];
+            p.ellipse(pt.x, pt.y, 10);
+            let x00 = p.lerp(pt.x, ptt0.x, t0);
+            let y00 = p.lerp(pt.y, ptt0.y, t0);
+            let x01 = p.lerp(pt.x, ptt0.x, t1);
+            let y01 = p.lerp(pt.y, ptt0.y, t1);
+            let x10 = p.lerp(pt.x, ptt1.x, t0);
+            let y10 = p.lerp(pt.y, ptt1.y, t0);
+            let x11 = p.lerp(pt.x, ptt1.x, t1);
+            let y11 = p.lerp(pt.y, ptt1.y, t1);
+            let r = p.constrain(p.map(ptt0.y - pt.y, -100, 100, 0, 255), 0, 255);
+            let g = 97;
+            let b = p.constrain(p.map(ptt0.x - pt.x, -100, 100, 0, 255), 0, 255);
+            p.fill(r, g, b, (0.5 - Math.abs(t - 0.5)) * 0.5 * 255 * alpha);
+            p.noStroke();
+            p.beginShape();
+            p.vertex(x00, y00);
+            p.vertex(x01, y01);
+            p.vertex(x11, y11);
+            p.vertex(x10, y10);
+            p.endShape();
+            p.stroke(r, g, b, 64 * alpha);
+            p.beginShape();
+            p.vertex(pt.x, pt.y);
+            p.vertex(ptt0.x, ptt0.y);
+            p.vertex(ptt1.x, ptt1.y);
+            p.endShape(p.CLOSE);
         }
     }
 
@@ -54,7 +72,3 @@ module.exports = (p) => {
     }
 
 }
-
-// export default function initP5() {
-//     return new p5(sketch);
-// }
